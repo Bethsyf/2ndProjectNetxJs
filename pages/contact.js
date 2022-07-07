a86d4a60a28b1d0708e54e9e95bd2d57b8d12fcf
Bethsyf <bethsyfalcon@gmail.com>
2022-07-07 15:18:02 -0500

    layout

--- pages/contact.js
+++ pages/contact.js
@@ -1,30 +1,18 @@
-import Head from 'next/head'
 import Link from "next/link"
-import Navbar from '../components/Navbar'
-import styles from '../styles/Home.module.css'
+import MainLayout from "../components/layouts/MainLayout"
 
 export default function ContactPage() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Contact</title>
-        <meta name="description" content="About Page" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Navbar/>
-
-      <main className={styles.main}>
+    <MainLayout>
       <h1>Contact Page</h1>
-        <h1 className={styles.title}>
-          Ir a <Link href="/">Home</Link>
-        </h1>
+      <h1 className={'title'}>
+        Ir a <Link href="/">Home</Link>
+      </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/contact.js</code>
-        </p>        
-      </main>      
-    </div>
+      <p className={'description'}>
+        Get started by editing {' '}
+        <code className={'code'}>pages/contact.js</code>
+      </p>
+    </MainLayout>
   )
 }
