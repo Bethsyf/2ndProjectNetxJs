c93249a17980d944563fc4e1d1c8ad728bbe17f0
Bethsyf <bethsyfalcon@gmail.com>
2022-07-07 18:09:49 -0500

    add pricing

--- pages/about.js
+++ pages/about.js
@@ -1,18 +1,29 @@
 import Link from "next/link"
+import { DarkLayout } from "../components/layouts/Darklayout"
 import MainLayout from "../components/layouts/MainLayout"
 
 export default function AboutPage() {
   return (
-    <MainLayout>
-      <h1>About Page</h1>
-      <h1 className={'title'}>
-        Ir a <Link href="/">Home</Link>
-      </h1>
+    <>      
+        <h1>About Page</h1>
+        <h1 className={'title'}>
+          Ir a <Link href="/">Home</Link>
+        </h1>
 
-      <p className={'description'}>
-        Get started by editing {' '}
-        <code className={'code'}>pages/about.js</code>
-      </p>
+        <p className={'description'}>
+          Get started by editing {' '}
+          <code className={'code'}>pages/about.js</code>
+        </p>      
+    </>
+  )
+}
+
+AboutPage.getLayout = function getLayout ( page ) {
+  return(
+    <MainLayout>
+      <DarkLayout>
+        { page }
+      </DarkLayout>
     </MainLayout>
   )
 }
